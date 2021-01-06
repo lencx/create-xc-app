0eebb49a5bc70b410ce4ada1257eab0a0c6d7cac
lencx <cxin1314@gmail.com>
2021-01-06 19:41:52 +0800

    chore: wasm deploy

--- template-wasm-react/src/App/index.tsx
+++ template-wasm-react/src/App/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { greet } from 'rust_crate'
 import logo from './logo.svg'
-import './App.css'
+import './index.css'
 
 function App() {
   return (
--- template-wasm-react/vite.deploy.ts
+++ template-wasm-react/vite.deploy.ts
@@ -1,27 +1,19 @@
 import * as reactPlugin from 'vite-plugin-react';
 import type { UserConfig } from 'vite';
 import path from 'path';
-const rustPlugin = require('vite-plugin-rust');
 
 const config: UserConfig = {
   jsx: 'react',
   minify: 'esbuild',
   plugins: [
     reactPlugin,
-    // https://github.com/gliheng/vite-plugin-rust
-    // TODO: `vite build` - *.wasm files loss
-    // Temporary solutions: scripts/wasm-copy.js
-    rustPlugin({
-      crates: {
-        rust_crate: './rust-crate',
-      },
-    }),
   ],
   // see: https://github.com/vitejs/vite/blob/master/src/node/config.ts
   // the key must start and end with a slash
   alias: {
     '/@/': path.resolve(__dirname, 'src'),
     '/hooks/': path.resolve(__dirname, 'src/hooks'),
+    'rust_crate': path.resolve(__dirname, 'rust-crate/pkg/rust_crate.js'),
   },
 };
 
